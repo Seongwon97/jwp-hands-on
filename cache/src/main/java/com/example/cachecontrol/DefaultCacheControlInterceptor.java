6e028d10a156be1fe8f91ad69fa492453916669d
Seongwon97 <osw970325@naver.com>
2022-09-02 13:30:50 +0900

    feat: 인터셉터를 통한 휴리스틱 캐싱 제거

---
+++ cache/src/main/java/com/example/cachecontrol/DefaultCacheControlInterceptor.java
@@ -0,0 +1,24 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class DefaultCacheControlInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) {
+        if (!response.containsHeader(HttpHeaders.CACHE_CONTROL)) {
+            final String cacheControl = CacheControl
+                    .noCache()
+                    .cachePrivate()
+                    .getHeaderValue();
+            response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+        }
+    }
+}
