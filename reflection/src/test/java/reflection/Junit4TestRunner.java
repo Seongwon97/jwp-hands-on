8ff1152c572d8e6a1402bb4ab21a83db82c0b4ad
Seongwon97 <osw970325@naver.com>
2022-09-16 16:20:53 +0900

    feat: Reflection 구현하기

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +12,12 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            MyTest declaredAnnotation = declaredMethod.getDeclaredAnnotation(MyTest.class);
+            if (declaredAnnotation != null) {
+                declaredMethod.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,16 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +21,11 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.getConstructorsAnnotatedWith(Controller.class)
+                .forEach(c -> log.info(c.getClass().getName()));
+        reflections.getConstructorsAnnotatedWith(Service.class)
+                .forEach(c -> log.info(c.getClass().getName()));
+        reflections.getConstructorsAnnotatedWith(Repository.class)
+                .forEach(c -> log.info(c.getClass().getName()));
     }
 }
